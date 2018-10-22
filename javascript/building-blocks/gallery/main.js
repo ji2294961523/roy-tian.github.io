16f40799444feabcadd787ce01eb0dbb3ab6511d
Roy <37997454+Roy-Tian@users.noreply.github.com>
2018-10-22 20:55:11 +0800

    Update main.js
    汉化

--- javascript/building-blocks/gallery/main.js
+++ javascript/building-blocks/gallery/main.js
@@ -4,7 +4,7 @@ var thumbBar = document.querySelector('.thumb-bar');
 btn = document.querySelector('button');
 var overlay = document.querySelector('.overlay');
 
-/* Looping through images */
+/* 遍历图片并添加至缩略图区 */
 
 for(var i = 1; i <= 5; i++) {
   var newImage = document.createElement('img');
@@ -20,17 +20,17 @@ function displayImage(imgSrc) {
   displayedImage.setAttribute('src', imgSrc);
 }
 
-/* Wiring up the Darken/Lighten button */
+/* 编写 变亮/变暗 按钮 */
 
 btn.onclick = function() {
   var btnClass = btn.getAttribute('class'); 
   if(btnClass === 'dark') {
     btn.setAttribute('class','light');
-    btn.textContent = 'Lighten';
+    btn.textContent = '变亮';
     overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
   } else {
     btn.setAttribute('class','dark');
-    btn.textContent = 'Darken';
+    btn.textContent = '变暗';
     overlay.style.backgroundColor = 'rgba(0,0,0,0)';
   }
 }
