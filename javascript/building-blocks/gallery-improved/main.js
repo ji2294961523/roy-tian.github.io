8738b81c074c6e6b3aad7bc04291dadb7f33aae4
Roy <37997454+Roy-Tian@users.noreply.github.com>
2018-10-25 15:58:29 +0800

    Add files via upload

--- javascript/building-blocks/gallery-improved/main.js
+++ javascript/building-blocks/gallery-improved/main.js
@@ -1,13 +1,11 @@
-const TOTAL_IMAGES = 6;
+const TOTAL_IMAGES = 10;
 const SLIDE_SIZE = 4;
 let images = [];
 let slideFirstIndex = 0;
+let divFull = document.getElementById('div-full');
 let btnBrightness = document.getElementById('btn-brightness');
-let imageCurrent = document.getElementById('img-current');
-let slideLeft = document.getElementById('div-slide-left');
-let slideRight = document.getElementById('div-slide-right');
-let test = document.getElementById('test');
-
+let btnBlur = document.getElementById('btn-blur');
+let btnGray = document.getElementById('btn-gray');
 
 for (let i = 0; i < TOTAL_IMAGES; i++) {
     images[i] = 'images/pic' + i + '.jpg';
@@ -15,32 +13,63 @@ for (let i = 0; i < TOTAL_IMAGES; i++) {
 
 function deploySlide(index = 0) {
     for (let i = 0; i < SLIDE_SIZE; i++) {
-        document.getElementById('img-slide-' + i).src = images[i + index];
+        let currentThumb = document.getElementById('div-thumb-' + i);
+        currentThumb.style.backgroundImage = 'url(' + images[i + index] + ')';
     }
 }
 
 deploySlide();
 
-slideLeft.onclick = function slideLeft() {
+document.getElementById('div-nav-left').onclick =  () => {
     if (slideFirstIndex > 0)
         deploySlide(--slideFirstIndex);
 };
 
-slideRight.onclick = function slideRight() {
+document.getElementById('div-nav-right').onclick =  () => {
     if (slideFirstIndex < TOTAL_IMAGES - SLIDE_SIZE)
         deploySlide(++slideFirstIndex);
 };
 
-btnBrightness.onclick = function () {
-    if (btnBrightness.textContent === '变暗')  {
-        imageCurrent.style.filter = 'brightness(0.3)';
+HTMLElement.prototype.setFilter = function (name, value) {
+    this.style.filter =
+        window.getComputedStyle(this, null)
+            .getPropertyValue('filter')
+            .replace(new RegExp(name + '\\\((.*?)\\\)'),
+                     name + '(' + value + ')');
+};
+
+btnBrightness.onclick = () => {
+    if (btnBrightness.textContent === '变暗') {
+        divFull.setFilter('brightness', 0.3);
         btnBrightness.textContent = '变亮';
     } else {
-        imageCurrent.style.filter = 'brightness(1)';
+        divFull.setFilter('brightness', 1);
         btnBrightness.textContent = '变暗';
     }
 };
 
-document.querySelectorAll('.img-slide').forEach(value => {
-    value.onclick = () => imageCurrent.src = value.src;
+btnBlur.onclick = () => {
+    if (btnBlur.textContent === '模糊') {
+        divFull.setFilter('blur', '10px');
+        btnBlur.textContent = '清晰';
+    } else {
+        divFull.setFilter('blur', '0px');
+        btnBlur.textContent = '模糊';
+    }
+};
+
+btnGray.onclick = () => {
+    if (btnGray.textContent === '黑白') {
+        divFull.setFilter('grayscale', 1);
+        btnGray.textContent = '彩色';
+    } else {
+        divFull.setFilter('grayscale', 0);
+        btnGray.textContent = '黑白';
+    }
+};
+
+document.querySelectorAll('.div-thumb').forEach(value => {
+    value.onclick =
+        () => divFull.style.backgroundImage = value.style.backgroundImage;
 });
+
