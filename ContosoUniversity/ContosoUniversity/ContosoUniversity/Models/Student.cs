e1473499e9078efdccdbfc31e233b84a9c4f4e75
ji2294961523 <2294961523@qq.com>
2019-01-07 21:33:56 +0800

    一月七号

---
+++ ContosoUniversity/ContosoUniversity/ContosoUniversity/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+      [Display(Name ="姓名")]
+       public string Name { get; set; }
+       [Display(Name ="注册日期")]
+        public DateTime EnrollmentDate { get; set; }
+        public string Secret { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
